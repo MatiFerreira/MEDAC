2f53f8f816840a81e9aab515838ce6914f07a96e
Mathias Ferreira Ferreira <maziuseven@gmail.com>
2022-10-08 15:36:21 +0200

    Termine el ejercicio 10 y el 4 a sido modificado :)

--- FileReader-FileWriter/src/ej10.java
+++ FileReader-FileWriter/src/ej10.java
@@ -1,10 +1,42 @@
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class ej10 {
-    static File fFile = null;
+
     public static void main(String[] args) {
-         fFile = new File("./mezcla.txt ");
+        try {
+            BufferedWriter bfwWriter = new BufferedWriter (new FileWriter("./mezcla.txt"));
+            BufferedWriter bfwWriter2 = new BufferedWriter (new FileWriter("./texto1.txt"));
+            BufferedWriter bfwWriter3 = new BufferedWriter (new FileWriter("./texto2.txt"));
+            //BufferedReader bfrReader = new BufferedReader(new FileReader("./mezcla.txt"));
+            BufferedReader bfrReader2 = new BufferedReader(new FileReader("./texto1.txt"));
+            BufferedReader bfrReader3 = new BufferedReader(new FileReader("./texto2.txt"));
+            //Añadimos texto a cada archivo
+            bfwWriter2.write("Hola");
+            bfwWriter3.write("Me llamo mathias");
+            bfwWriter3.close();
+            bfwWriter2.close();
+            String textos = bfrReader2.readLine();
+            String textos2 = bfrReader3.readLine();
+            int posicion = 1;
+            while (posicion != 5){
+                if (posicion%2==0){
+                    bfwWriter.write(textos2+"\n");
+                }else {
+                    bfwWriter.write(textos+"\n");
+                }
+                bfwWriter.flush();// un limpiador de buffer para poder escribir nuevamente :)
+                posicion++;
+            }
+
+
+            bfwWriter.close();
+            bfrReader3.close();
+            bfrReader2.close();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
