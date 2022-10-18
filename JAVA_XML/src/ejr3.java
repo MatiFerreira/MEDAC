3dbc12f73db4c5e93c3ac98e7c9d3ef062b18509
Mathias Ferreira Ferreira <maziuseven@gmail.com>
2022-10-18 20:23:07 +0200

    He hecho las actividades de xml con bastante difucultad en el numero 3 por lo demas esta bien creo yo

--- JAVA_XML/src/ejr3.java
+++ JAVA_XML/src/ejr3.java
@@ -17,6 +17,7 @@ public class ejr3 {
             DocumentBuilderFactory dbfFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dbBuild = dbfFactory.newDocumentBuilder();
             Document docDocument = dbBuild.parse(new File("equipo_futbol.xml"));
+            docDocument.normalizeDocument();
             NodeList ndlNodelist = docDocument.getElementsByTagName("equipo");
 
             for (int i=0;i<ndlNodelist.getLength();i++){
@@ -24,22 +25,46 @@ public class ejr3 {
                 Node nNodes = ndlNodelist.item(i);
                 Element eElemet =(Element)nNodes;
                 NodeList nlListSoon = eElemet.getChildNodes();
+                //FOR QUE RECORREMOS PARA VER LOS ARCHIVOS XML
                     for (int k=0;k<nlListSoon.getLength();k++){
                         Node nNodesoon = nlListSoon.item(k);
                         if (nNodesoon.getNodeType()== Node.ELEMENT_NODE && "nombre".equals(nNodesoon.getNodeName())){
-                            System.out.println(nNodesoon.getNodeName() +": "+nNodesoon.getTextContent());
+                            System.out.println(nNodesoon.getNodeName() +" de la club : "+nNodesoon.getTextContent());
                         }
 
                         if (nNodesoon.getNodeType()== Node.ELEMENT_NODE && "ciudad".equals(nNodesoon.getNodeName())){
                             System.out.println(nNodesoon.getNodeName() +": "+nNodesoon.getTextContent());
                         }
-
                         if (nNodesoon.getNodeType()== Node.ELEMENT_NODE && "entrenador".equals(nNodesoon.getNodeName())){
-                            System.out.println(nNodesoon.getNodeName() +": "+nNodesoon.getTextContent());
+                                if(nNodesoon.getTextContent().equals("")){
+                                    System.out.println(nNodesoon.getNodeName() +": "+"SIN ENTRENADOR :(");
+                            }else {
+                                    System.out.println(nNodesoon.getNodeName() +": "+nNodesoon.getTextContent());
+                                }
+
                         }
 
-                    }
+                        NodeList nodelistjugador = nNodesoon.getChildNodes();
+                        for (int m= 0; m<nodelistjugador.getLength();m++){//jugadores
+                                Node nodesjugador = nodelistjugador.item(m);
+                                NodeList listajugador = nodesjugador.getChildNodes();
 
+                                for (int x=0;x< listajugador.getLength();x++){//jugador
+                                    Node jugadorenodes = listajugador.item(x);
+                                    Element elementojugador = (Element) listajugador;
+
+                                    if (jugadorenodes.getNodeType()== Node.ELEMENT_NODE && "nombre".equals(jugadorenodes.getNodeName())){
+                                        System.out.print("su posicion actual en el campo de futbol es: ");
+                                        System.out.println(elementojugador.getAttribute("posicion"));
+                                        System.out.println(jugadorenodes.getNodeName() +": "+jugadorenodes.getTextContent());
+                                    }
+                                    if (jugadorenodes.getNodeType()== Node.ELEMENT_NODE && "nacionalidad".equals(jugadorenodes.getNodeName())){
+                                        System.out.println(jugadorenodes.getNodeName() +": "+jugadorenodes.getTextContent());
+                                    }
+                                }
+                        }
+                    }
+                System.out.println("-----------------------------------");
             }
 
 
